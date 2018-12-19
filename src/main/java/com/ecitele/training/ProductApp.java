cbe4c73dc575dd4ebe9859a880e4baa4efdd6e1b
hnaik <hnaik@HNAIK01.ecitele.com>
2018-12-19 13:20:08 +0530

    int a added.

--- src/main/java/com/ecitele/training/ProductApp.java
+++ src/main/java/com/ecitele/training/ProductApp.java
@@ -7,6 +7,8 @@ import com.ecitele.training.ui.ProductConsoleUI;
 
 public class ProductApp {
 	
+	
+	int a;
 	public static void main(String[] args) {
 		@SuppressWarnings("resource")
 		ApplicationContext context = new ClassPathXmlApplicationContext("spring-application.xml");
